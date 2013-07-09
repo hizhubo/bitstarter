9e5a50625cc700ffa55ea0bfa31930c3560f0803
Bo Zhu <hizhubo@gmail.com>
2013-07-09 15:17:01 +0000

    Read content from index.html

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html', function (err, data) {
+    var content;
+
+    if (err) {
+      content = err;
+    } else {
+      content = data;
+    }
+
+    response.send(conten);
+  }
 });
 
 var port = process.env.PORT || 5000;
