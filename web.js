709bfb22f5ed2bd9c597c73a7acfe3c78c500ff6
Bo Zhu <hizhubo@gmail.com>
2013-07-14 02:57:39 +0000

    Fix error 'no such file or directory index.html'. Modified port to 8080

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFileSync('index.html', 'utf-8');
+  var content = fs.readFileSync('index.html').toString();
   /*
   fs.readFileSync('index.html', 'utf-8', function (err, data) {
     var content;
@@ -21,7 +21,7 @@ app.get('/', function(request, response) {
   response.send(content);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
